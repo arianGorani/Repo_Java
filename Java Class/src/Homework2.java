d379acf923497d249265ac7aa1de2fa9dd3376f5
Arian Gorani <you@example.com>
2021-02-23 12:12:24 -0500

    Homework 2

--- Java Class/src/Homework2.java
+++ Java Class/src/Homework2.java
@@ -2,16 +2,6 @@ public class Homework2 {
     public static void main(String[] args) {
 
 
-        /**Store below scenario value in an appropriate dataType variable, and print the same in console.
-         1. number of hours in a day (byte)
-         2. max no. of days in an year
-         3. total number of employees in an organization
-         4. population in a country
-         5. interest rate
-         6. balance in a bank account
-         7. does the sun rise from the west? Store answer
-         8. initials of your name */
-
 
         byte numHoursDay = 24;              // 1. number of hours in a day (byte)
         short maxNumOfYear = 365;           // 2. max no. of days in an year
@@ -22,6 +12,18 @@ public class Homework2 {
         boolean sunRiseWest = false;        // 7. does the sun rise from the west? Store answer
         char firstNameInitials = 'A';       // 8. initials of your first name
         char lastNameInitial = 'G';         // 9. initials of your last name
+
+        System.out.println("number of hours in a day " + numHoursDay);
+        System.out.println("max no. of days in an year " + maxNumOfYear);
+        System.out.println("total number of employees in an organization " + totNumOfEmployees);
+        System.out.println("population in a country " + usaPopulation);
+        System.out.println("interest rate " + interestRate);
+        System.out.println("balance in a bank account " + balanceAccount);
+        System.out.println("does the sun rise from the west? Store answer " + sunRiseWest);
+        System.out.println("initials of your first name " + firstNameInitials);
+        System.out.println("initials of your last name " + lastNameInitial);
+
+
 
     }
 }
