2d2f2ca0fe6158a19971726315dcf34b54683012
Arian Gorani <you@example.com>
2021-02-22 22:05:37 -0500

    Homework2

---
+++ Java Class/src/Homework2.java
@@ -0,0 +1,27 @@
+public class Homework2 {
+    public static void main(String[] args) {
+
+
+        /**Store below scenario value in an appropriate dataType variable, and print the same in console.
+         1. number of hours in a day (byte)
+         2. max no. of days in an year
+         3. total number of employees in an organization
+         4. population in a country
+         5. interest rate
+         6. balance in a bank account
+         7. does the sun rise from the west? Store answer
+         8. initials of your name */
+
+
+        byte numHoursDay = 24;              // 1. number of hours in a day (byte)
+        short maxNumOfYear = 365;           // 2. max no. of days in an year
+        byte totNumOfEmployees = 25;        // 3. total number of employees in an organization
+        long usaPopulation = 328200000L;    // 4. population in a country
+        float interestRate = 3.4F;          // 5. interest rate
+        double balanceAccount = 13456.73;   // 6. balance in a bank account
+        boolean sunRiseWest = false;        // 7. does the sun rise from the west? Store answer
+        char firstNameInitials = 'A';       // 8. initials of your first name
+        char lastNameInitial = 'G';         // 9. initials of your last name
+
+    }
+}
