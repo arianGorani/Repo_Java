3a259fb2a65b0401b0841efd5d050abff9e2fccb
Arian Gorani <you@example.com>
2021-03-02 13:03:07 -0500

    Exercise

--- Java Class/src/arian.java
+++ Java Class/src/arian.java
@@ -1,6 +1,9 @@
+import java.util.Locale;
+
 public class arian {
 
     public static void main(String[] args) {
+
         int i = 5;
         int k =10;
 
@@ -10,15 +13,37 @@ public class arian {
 
         System.out.println(i);      // 6
         System.out.println(k);      // 11
-        System.out.println(j);      // 16
-        System.out.println("\n");
+        System.out.println(j + "\n");      // 16
+
 
         int g = ++i + k++ + ++j;
         System.out.println(i);  // i = 7
         System.out.println(k);  // k = 12
-        System.out.println(j);  // j = 17
+        System.out.println(j + "\n");  // j = 17
 
 
         System.out.println(g);      // g = 35
+
+        int creditScore = 701;
+        boolean isFirstHomeBuyer = true;
+
+        double buingHome = creditScore > 700 && isFirstHomeBuyer ? 1.5 : 2.0;
+        System.out.println("This buyer is eligible for " + buingHome + " Interes rate\n");
+
+        int age = 25;
+        boolean haveId = true;
+
+        String enterClub = age >= 18 && haveId ?  "You can enter the Club\n" : "You cannot enter the club\n";
+        System.out.println(enterClub);
+
+        int age1 = 20;
+        boolean usCitizen = true;
+        String eligibleToVote = age1 >=18 && usCitizen ? "You can VOTE\n" : "You cannot vote\n";
+        System.out.println(eligibleToVote);
+
+        System.out.println(eligibleToVote.toUpperCase());
+        System.out.println(enterClub.toLowerCase());
+
+
     }
 }
