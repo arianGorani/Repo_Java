bd28bb1ae41956abbd4d1f74aa571c041b446561
Arian Gorani <arian.gorani@gmail.com>
2021-03-19 00:21:34 -0400

    Homework 7

--- Java Class/src/Homework7.java
+++ Java Class/src/Homework7.java
@@ -4,18 +4,21 @@ public class Homework7 {
 
         // Due: Mar 18, 2021
         /**     TASK 1
-         * String sentence1 =  "hApPY nEW yeAr ";
+         * String sentence1 =  "hApPY nEW yeAr";
          * sout(sentence1);  // hApPY nEW yeAr
          * // code
          * sout(sentence1);  // Happy New Year
          */
         
         String sentence1 = "hApPY nEW yeAr";
+        System.out.println(sentence1);
         String[] sen1ArrayString = sentence1.toLowerCase().split(" ");
+        sentence1 ="";
         for (int i = 0; i <= sen1ArrayString.length -1; i++){
-            System.out.print(sen1ArrayString[i].toUpperCase().charAt(0));
-            System.out.print(sen1ArrayString[i].substring(1) + " ");
+             sentence1 = sentence1 + sen1ArrayString[i].toUpperCase().charAt(0) + sen1ArrayString[i].substring(1) + " ";
         }
+        sentence1 = sentence1.trim();
+        System.out.println(sentence1);
 
 
 
@@ -26,26 +29,28 @@ public class Homework7 {
          * LSC
          *
          */
-        System.out.println("\n");
         String sentence2 = "Lab sessIONS clAsses";
         String[] stringSplit = sentence2.split(" ");
+        sentence2 ="";
         for (int i = 0; i <= stringSplit.length -1; i++ ){
-            System.out.print(stringSplit[i].toUpperCase().charAt(0));
+           sentence2 = sentence2 + stringSplit[i].toUpperCase().substring(0,1);
         }
+        System.out.println(sentence2);
 
 
-        /**     TASK 3
+
+        /**
          * Reverse a string
          *
          */
-        System.out.println("\n");
         String sentence3 = "happy holidays";
-        String reverseSentence3 = "happy holidays";
-        System.out.println("sentence3: " + sentence3);     // happy holidays
-        System.out.print("Reverse sentence3: " );          // syadiloh yppah
-        for (int i = reverseSentence3.length()-1; i >= 0; i--){
-            System.out.print(reverseSentence3.charAt(i));
+        String reverseSentence3 = "";
+        System.out.println("sentence3 : " + sentence3);     // happy holidays
+        String[] sen3Array = sentence3.split("");
+        for (int i = 0; i < sen3Array.length; i++){
+            reverseSentence3 = reverseSentence3 + sen3Array[sen3Array.length-1-i];
         }
+        System.out.println("Reverse sentence3 : " + reverseSentence3);  // syadiloh yppah
 
     }
 }
